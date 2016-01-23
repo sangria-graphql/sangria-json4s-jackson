13b10182b36675d97b68392b7242bbe1fb2d52be
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-01-23 19:59:14 +0100

    tests

---
+++ src/test/scala/sangria/marshalling/Json4sJacksonSupportSpec.scala
@@ -0,0 +1,64 @@
+package sangria.marshalling
+
+import org.scalatest.{Matchers, WordSpec}
+
+import sangria.marshalling.json4s.jackson._
+import sangria.marshalling.testkit._
+
+import org.json4s.JsonAST._
+
+class Json4sJacksonSupportSpec extends WordSpec with Matchers with MarshallingBehaviour with InputHandlingBehaviour {
+  "Json4s native integration" should {
+    behave like `value (un)marshaller` (Json4sJacksonResultMarshaller)
+
+    behave like `AST-based input unmarshaller` (json4sJacksonFromInput[JValue])
+    behave like `AST-based input marshaller` (Json4sJacksonResultMarshaller)
+  }
+
+  val toRender = JObject(
+    "a" → JArray(List(JNull, JInt(123), JArray(List(JObject("foo" → JString("bar")))))),
+    "b" → JObject(
+      "c" → JBool(true),
+      "d" → JNull))
+
+  "InputUnmarshaller" should {
+    "throw an exception on invalid scalar values" in {
+      an [IllegalStateException] should be thrownBy
+          Json4sJacksonInputUnmarshaller.getScalarValue(JObject())
+    }
+
+    "throw an exception on variable names" in {
+      an [IllegalArgumentException] should be thrownBy
+          Json4sJacksonInputUnmarshaller.getVariableName(JString("$foo"))
+    }
+
+    "render JSON values" in {
+      val rendered = Json4sJacksonInputUnmarshaller.render(toRender)
+
+      rendered should be ("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
+    }
+  }
+
+  "ResultMarshaller" should {
+    "render pretty JSON values" in {
+      val rendered = Json4sJacksonResultMarshaller.renderPretty(toRender)
+
+      rendered.replaceAll("\r", "") should be (
+        """{
+          |  "a" : [ null, 123, [ {
+          |    "foo" : "bar"
+          |  } ] ],
+          |  "b" : {
+          |    "c" : true,
+          |    "d" : null
+          |  }
+          |}""".stripMargin.replaceAll("\r", ""))
+    }
+
+    "render compact JSON values" in {
+      val rendered = Json4sJacksonResultMarshaller.renderCompact(toRender)
+
+      rendered should be ("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
+    }
+  }
+}
