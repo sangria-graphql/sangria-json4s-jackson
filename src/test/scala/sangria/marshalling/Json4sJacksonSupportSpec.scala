6a65fe2526d40e5b7286eda931a2eebc3d08a193
sh0hei <vividbit@gmail.com>
2021-03-07 19:14:45 +0900

    Use sbt-scalafmt

--- src/test/scala/sangria/marshalling/Json4sJacksonSupportSpec.scala
+++ src/test/scala/sangria/marshalling/Json4sJacksonSupportSpec.scala
@@ -1,6 +1,5 @@
 package sangria.marshalling
 
-
 import sangria.marshalling.json4s.jackson._
 import sangria.marshalling.testkit._
 
@@ -8,44 +7,48 @@ import org.json4s.JsonAST._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class Json4sJacksonSupportSpec extends AnyWordSpec with Matchers with MarshallingBehaviour with InputHandlingBehaviour with ParsingBehaviour {
+class Json4sJacksonSupportSpec
+    extends AnyWordSpec
+    with Matchers
+    with MarshallingBehaviour
+    with InputHandlingBehaviour
+    with ParsingBehaviour {
   "Json4s native integration" should {
-    behave like `value (un)marshaller` (Json4sJacksonResultMarshaller)
+    behave.like(`value (un)marshaller`(Json4sJacksonResultMarshaller))
 
-    behave like `AST-based input unmarshaller` (json4sJacksonFromInput[JValue])
-    behave like `AST-based input marshaller` (Json4sJacksonResultMarshaller)
+    behave.like(`AST-based input unmarshaller`(json4sJacksonFromInput[JValue]))
+    behave.like(`AST-based input marshaller`(Json4sJacksonResultMarshaller))
 
-    behave like `input parser` (ParseTestSubjects(
-      complex = """{"a": [null, 123, [{"foo": "bar"}]], "b": {"c": true, "d": null}}""",
-      simpleString = "\"bar\"",
-      simpleInt = "12345",
-      simpleNull = "null",
-      list = "[\"bar\", 1, null, true, [1, 2, 3]]",
-      syntaxError = List("[123, \"FOO\" \"BAR\"")
-    ))
+    behave.like(
+      `input parser`(ParseTestSubjects(
+        complex = """{"a": [null, 123, [{"foo": "bar"}]], "b": {"c": true, "d": null}}""",
+        simpleString = "\"bar\"",
+        simpleInt = "12345",
+        simpleNull = "null",
+        list = "[\"bar\", 1, null, true, [1, 2, 3]]",
+        syntaxError = List("[123, \"FOO\" \"BAR\"")
+      )))
   }
 
   val toRender = JObject(
     "a" -> JArray(List(JNull, JInt(123), JArray(List(JObject("foo" -> JString("bar")))))),
-    "b" -> JObject(
-      "c" -> JBool(true),
-      "d" -> JNull))
+    "b" -> JObject("c" -> JBool(true), "d" -> JNull))
 
   "InputUnmarshaller" should {
     "throw an exception on invalid scalar values" in {
-      an [IllegalStateException] should be thrownBy
-          Json4sJacksonInputUnmarshaller.getScalarValue(JObject())
+      an[IllegalStateException] should be thrownBy
+        Json4sJacksonInputUnmarshaller.getScalarValue(JObject())
     }
 
     "throw an exception on variable names" in {
-      an [IllegalArgumentException] should be thrownBy
-          Json4sJacksonInputUnmarshaller.getVariableName(JString("$foo"))
+      an[IllegalArgumentException] should be thrownBy
+        Json4sJacksonInputUnmarshaller.getVariableName(JString("$foo"))
     }
 
     "render JSON values" in {
       val rendered = Json4sJacksonInputUnmarshaller.render(toRender)
 
-      rendered should be ("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
+      rendered should be("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
     }
   }
 
@@ -53,8 +56,7 @@ class Json4sJacksonSupportSpec extends AnyWordSpec with Matchers with Marshallin
     "render pretty JSON values" in {
       val rendered = Json4sJacksonResultMarshaller.renderPretty(toRender)
 
-      rendered.replaceAll("\r", "") should be (
-        """{
+      rendered.replaceAll("\r", "") should be("""{
           |  "a" : [ null, 123, [ {
           |    "foo" : "bar"
           |  } ] ],
@@ -68,7 +70,7 @@ class Json4sJacksonSupportSpec extends AnyWordSpec with Matchers with Marshallin
     "render compact JSON values" in {
       val rendered = Json4sJacksonResultMarshaller.renderCompact(toRender)
 
-      rendered should be ("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
+      rendered should be("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
     }
   }
 }
